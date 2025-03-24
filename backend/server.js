0c145710e1ba33d1b14bfad2ed86a273f0771184
ankmay0 <153919929+ankmay0@users.noreply.github.com>
2025-03-24 12:45:47 +0530

    modified for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
 
 app.use(
   cors({
-    origin: "https://cgu-marketplace.onrender.com", // ✅ Correct origin
+    origin: "https://cgumarketplacee.onrender.com", // ✅ Correct origin
     credentials: true, // ✅ If using cookies or sessions
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
