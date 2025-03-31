7da1eceddea5f72df0065617305c80653694c5d0
shankhanraman <shankhan.raman@gmail.com>
2025-04-01 05:07:07 +0530

    improved nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -53,6 +53,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/terms" element={<Terms />} />
