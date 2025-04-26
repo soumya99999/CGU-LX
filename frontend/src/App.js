327aa9048a7a7a8bb2e6e15c29509ccee80e29fd
Mayank <workformayank0@gmail.com>
2025-04-26 22:36:37 +0530

    improved SEO and cleaned app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import ReactGA from "react-ga4";
-import { Analytics } from "@vercel/analytics/react"
-import React, { useState, useEffect } from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from "react";
 
+//import pages
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Sell from "./pages/Sell";
 import Buy from "./pages/Buy";
@@ -22,10 +19,10 @@ import Terms from "./components/terms";
 import Privacy from "./components/privacy";
 import HowItWorks from "./components/HowItWorks";
 import CommunityGuidelines from "./components/CommunityGuidelines";
-ReactGA.initialize("G-XXXXXXXXXX");
+
+ReactGA.initialize("G-VTT700DPSY");
 function App() {
-  const [fade, setFade] = useState(false);
-  const [showContent, setShowContent] = useState(true);
+
   const location = useLocation();
 
   useEffect(() => {
@@ -33,48 +30,28 @@ function App() {
       hitType: "pageview", 
       page: location.pathname + location.search 
     });
-    setFade(true);
-    setShowContent(false);
-
-    const fadeOutTimeout = setTimeout(() => {
-      setShowContent(true);
-      setFade(false);
-    }, 600);
-
-    return () => clearTimeout(fadeOutTimeout);
   }, [location]);
-
+  
   return (
     <div className="relative">
-      {/* Blurred transition overlay */}
-      {/* <div 
-        className={`fixed inset-0 bg-white/80 backdrop-blur-lg transition-opacity duration-[800ms] ease-in-out ${
-          fade ? "opacity-100" : "opacity-0"
-        } pointer-events-none z-50`}
-      ></div> */}
-
-      {/* Smooth fade for content */}
-      {/* <div className={`app-content  pt-[57px]  transition-opacity duration-[1000ms] ease-in-out ${fade ? "opacity-0" : "opacity-100"}`}> */}
       <div className={`app-content  pt-[57px] `}>
         <Navbar />
-        {showContent && (
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/how-it-works" element={<HowItWorks />} />
-            <Route path="/community-guidelines" element={<CommunityGuidelines />} />
-            <Route path="/sell" element={<ProtectedRoute><Sell /></ProtectedRoute>} />
-            <Route path="/buy" element={<ProtectedRoute><Buy /></ProtectedRoute>} />
-            <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-            <Route path="/edit-product/:productId" element={<ProtectedRoute><EditProduct /></ProtectedRoute>} />
-          </Routes>
-        )}
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/how-it-works" element={<HowItWorks />} />
+              <Route path="/community-guidelines" element={<CommunityGuidelines />} />
+              <Route path="/sell" element={<ProtectedRoute><Sell /></ProtectedRoute>} />
+              <Route path="/buy" element={<ProtectedRoute><Buy /></ProtectedRoute>} />
+              <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path="/edit-product/:productId" element={<ProtectedRoute><EditProduct /></ProtectedRoute>} />
+            </Routes>
         <Footer />
       </div>
     </div>
